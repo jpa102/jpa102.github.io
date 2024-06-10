49160d851dac6f003e46794e1d83b1fcb91d9024
John Patrick Adem <161832897+jpa102@users.noreply.github.com>
2024-06-10 20:23:17 +0800

    Add files via upload
    add the javascript files for faketube frontend (2022)

---
+++ faketube/2022/rsrc/js/globaldata_online.js
@@ -0,0 +1,66 @@
+﻿// globaldata_online.js
+// this file contains code that requires internet connection
+
+/*
+	votes (likes, dislikes, and rating)
+*/
+v = ""; // video id variable (11-character string)
+
+function getVideoIdFromUser() {
+	v = prompt("Enter a 11-character long YouTube video id\nExample is dQw4w9WgXcQ\n\n\(From https://www.youtube.com/watch?v=dQw4w9WgXcQ\)");
+}
+
+while (v == "") {
+	getVideoIdFromUser();
+	if (v == "") {
+		alert("Please enter a video id first");
+	}
+}
+
+let statsSet = false;
+
+fetch(
+	`https://returnyoutubedislikeapi.com/votes?videoId=${v}`
+	).then((response) => {
+		response.json().then((json) => {
+			if (json) {
+				let { dislikes, likes, viewCount, id } = json;
+				console.log("Data provided by Return YouTube Dislike API\nLink to the API: https://returnyoutubedislikeapi.com\n\nVideo ID: " + id + "\nViews: " + viewCount + "\nLike count: " + likes + "\nDislike count: " + dislikes)
+			}
+		})
+	}
+);
+
+
+
+fetch(
+	`https://noembed.com/embed?url=https://www.youtube.com/watch?v=${v}`
+	).then((response) => {
+		response.json().then((json) => {
+			if (json) {
+				let { title, author_name, author_url } = json;
+				
+				ytVideoTitle = title;
+				ytChannelName = author_name;
+				ytChannelIdLink = author_url;
+				ytChannelSubConfirmLink = author_url + "?sub_confirmation=1";
+			}
+		})
+	}
+);
+
+
+
+/*
+	views and date
+*/
+
+uploaddate = "Month Day, Year";
+uploadtime = "T00:00:00-00:00";
+
+/*
+	yt info
+*/
+ytVideoId = v;
+ytSubscriberCount = "0";
+ytOnlineProfilePicture = "rsrc/player/YourVideoId.jpg";
---
+++ faketube/2022/rsrc/js/setytchannelinfo.js
@@ -0,0 +1,29 @@
+/*
+	sets the YouTube channel info
+*/
+
+// i have no idea why i have to set up setTimeout every fucking time
+setTimeout(function(){
+	// remove the "failed to load yt channel" in the html
+	document.querySelector("#yt-channel-failed-to-load").remove();
+
+	// set up the channel with proper data
+	document.querySelector("#yt-channel-container").insertAdjacentHTML(
+		"beforeend",
+		`
+		<div id="yt-channel">
+			<div id="yt-channel-image">
+				<img id="yt-channel-profile-picture" src="${ytOnlineProfilePicture}">
+			</div>
+			<div id="yt-channel-name-renderer">
+				<p id="yt-channel-name">
+					<a id="yt-channel-link" href="${ytChannelIdLink}">${ytChannelName}</a>
+				</p>
+				<p class="yt-channel-sub-count">
+					<a class="yt-channel-sub-count" href="${ytChannelSubConfirmLink}" title="Subscribe to ${ytChannelName} (YouTube)">${ytSubscriberCount} subscribers</a>
+				</p>
+			</div>
+		</div>
+		`
+	);
+}, 250);
