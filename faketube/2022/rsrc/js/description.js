0390fc8dca2087f606900a0c449c435b2898b24e
John Patrick Adem <161832897+jpa102@users.noreply.github.com>
2024-06-28 00:11:44 +0800

    Add files via upload
    upload the updated 2022 layout page and introduce a working homepage

---
+++ faketube/2022/rsrc/js/description.js
@@ -0,0 +1,56 @@
+﻿/*
+	description.js
+	
+	this modifies the preview and full description
+	this is in alpha version so expect bugs
+*/
+
+function descriptionButton() {
+	// show more
+	if (document.querySelector("#expand-description-button").ariaPressed == "false") {
+		// this is where you will put the full description
+		document.querySelector("#description-text").innerHTML =
+		`
+			Your Description here
+			<br>
+			This is a description
+			<br>
+			<br>
+			that contains text
+			<br>
+			<br>
+			about the video
+			<br>
+			<br>
+			<br>
+			This is just an example of what it looks like. You can choose to edit the description manually
+			<br>
+			with a text editor or open this in a browser, press f12, go to console, and copy this code:
+			<br>
+			<br>
+			document.designMode = 'on'
+			<br>
+			<br>
+			After you're done, you can turn it off with this code:
+		`
+		
+		document.querySelector("#expand-description-button").innerText = "Show less";
+		document.querySelector("#expand-description-button").ariaPressed = "true";
+		return;
+	}
+
+	// show less
+	if (document.querySelector("#expand-description-button").ariaPressed == "true") {
+		document.querySelector("#description-text").innerHTML =
+		`
+			Your Description here
+			<br>
+			This is a description
+			<br>
+		`
+		
+		document.querySelector("#expand-description-button").innerText = "Show more";
+		document.querySelector("#expand-description-button").ariaPressed = "false";
+		return;
+	}
+}
---
+++ faketube/2022/rsrc/js/setratingbar.js
@@ -0,0 +1,8 @@
+/*
+	Set the ratio bar's width
+*/
+
+setTimeout(function(){
+	// ratio bar
+	document.querySelector("#like-bar").style = "width: " + likepercentage + "%";
+}, 1000); // waiting for 1 second...
