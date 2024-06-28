3ad17a4ce6a55b09a44da3cc775ae62151a41069
John Patrick Adem <161832897+jpa102@users.noreply.github.com>
2024-06-28 20:45:09 +0800

    description.js の更新
    make the values blank for each button press by default
    
    this can be filled by calling FetchAdditionalVideoMetadata()

--- faketube/2021/rsrc/js/description.js
+++ faketube/2021/rsrc/js/description.js
@@ -1,38 +1,18 @@
-﻿/*
+/*
 	description.js
 	
 	this modifies the preview and full description
-	this is in alpha version so expect bugs
+	by default, it has no data in it
 */
+
+var collapseddescription = "";
+var uncollapseddescription = "";
 
 function descriptionButton() {
 	// show more
 	if (document.querySelector("#expand-description-button").ariaPressed == "false") {
 		// this is where you will put the full description
-		document.querySelector("#description-text").innerHTML =
-		`
-			Your Description here
-			<br>
-			This is a description
-			<br>
-			<br>
-			that contains text
-			<br>
-			<br>
-			about the video
-			<br>
-			<br>
-			<br>
-			This is just an example of what it looks like. You can choose to edit the description manually
-			<br>
-			with a text editor or open this in a browser, press f12, go to console, and copy this code:
-			<br>
-			<br>
-			document.designMode = 'on'
-			<br>
-			<br>
-			After you're done, you can turn it off with this code:
-		`
+		document.querySelector("#description-text").innerText = uncollapseddescription;
 		
 		document.querySelector("#expand-description-button").innerText = "Show less";
 		document.querySelector("#expand-description-button").ariaPressed = "true";
@@ -41,13 +21,7 @@ function descriptionButton() {
 
 	// show less
 	if (document.querySelector("#expand-description-button").ariaPressed == "true") {
-		document.querySelector("#description-text").innerHTML =
-		`
-			Your Description here
-			<br>
-			This is a description
-			<br>
-		`
+		document.querySelector("#description-text").innerText = collapseddescription;
 		
 		document.querySelector("#expand-description-button").innerText = "Show more";
 		document.querySelector("#expand-description-button").ariaPressed = "false";
