5ca657c50127a217220cbc9e0a0a91d09924a6d1
John Patrick Adem <161832897+jpa102@users.noreply.github.com>
2024-07-31 01:01:43 +0800

    Add files via upload

---
+++ faketube/2017/rsrc/js/buttonslistener.js
@@ -0,0 +1,64 @@
+/*
+	==========================================================
+			Like and Dislike buttons
+	==========================================================
+*/
+function likeButton() {
+	likebuttonpressedcheck();
+}
+
+function dislikeButton() {
+	dislikebuttonpressedcheck();
+}
+
+//	get the width of like and dislike buttons for the ratio bar
+
+
+
+/*
+	==========================================================
+						Other buttons
+	==========================================================
+*/
+function shareButton() {
+	console.log("function not implemented. [ln:32 buttonlistener.js]");
+}
+
+function downloadButton() {
+	setTimeout(function() {
+		if (faketube.config_.EXPERIMENT_FLAGS.web_old_menu_buttons == true) {
+			document.querySelector("#download").style = "display: none !important"; // if the experimental flag for 2021 buttons is set to true
+		} else {
+			document.querySelector("#download-button").style = "display: none !important"; // default menu buttons
+		}
+	}, 200);
+	setTimeout(function() {
+		alert("Download unavailable");
+	}, 900);
+	console.log("sorry, no video download available.\n\nin fact, there's not even a single line of code here that provides download functionality. \(just kidding, set third_party_downloader_test variable to true\) [ln:38 buttonlistener.js]");
+
+}
+
+function saveButton() {
+	console.log("function not implemented. [ln:40 buttonlistener.js]");
+}
+
+function moreButton() {
+	console.log("function not implemented. [ln:44 buttonlistener.js]");
+}
+
+
+
+//	set the vote counts in the buttons
+setTimeout(function(){
+	// likes
+	document.querySelector("#like-count-renderer").innerText = likeCount.toLocaleString();
+	
+	// dislikes
+	document.querySelector("#dislike-count-renderer").innerText = dislikeCount.toLocaleString();
+	
+	// system tooltip for the ratio bar (average rating and percentage)
+	document.querySelector("ratio-bar-renderer").title = likepercentage + "% / " + averageRating;
+	
+	setRatioBar();
+}, 1150);
