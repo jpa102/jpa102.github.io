434fabd04171d034348c9f75aac032588181268a
John Patrick Adem <161832897+jpa102@users.noreply.github.com>
2024-08-05 00:34:44 +0800

    reupload the 2017 layout
    update the entire 2017 internal files (probably rehaul?)

---
+++ faketube/2017/rsrc/js/setviewsanddate.js
@@ -0,0 +1,56 @@
+﻿/*
+
+	set views and date of the video published
+
+*/
+
+fetch(
+	`https://returnyoutubedislikeapi.com/votes?videoId=${v}`
+	).then((response) => {
+		response.json().then((json) => {
+			if (json && !("traceId" in response) && !statsSet) {
+				let { viewCount } = json;
+				unformattedviews = viewCount;
+				totalviews = unformattedviews.toLocaleString();
+			}
+		})
+	}
+);
+
+setTimeout(function(){
+	document.querySelector("#failed-to-load-views-and-date").remove();
+	document.querySelector("#video-views").insertAdjacentHTML(
+
+		"beforeend",
+
+		`
+
+		<p id="video-metadata" class="video-metadata-renderer">
+
+			${totalviews} views
+
+		</p>
+		`
+	);
+	
+	document.querySelector("#date-published").innerText = uploaddate;
+	
+	// including the code in replacing the video title because why not? :D
+	
+	document.querySelector("#video-title-failed.video-title-failed-to-load").remove();
+	document.querySelector("#video-title-and-date-published").insertAdjacentHTML(
+		"afterbegin",
+		`
+		<div id="hashtags-container" hidden="">
+			<span class="video-hashtag"></span>
+			<span class="video-hashtag"></span>
+			<span class="video-hashtag"></span>
+		</div>
+		<h1 id="video" class="title">
+
+			${ytVideoTitle}
+
+		</h1>
+		`
+	);
+}, 1150);
