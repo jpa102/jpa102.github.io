86ed7077dbb3917d3f725c17cf6236f8f3cde691
John Patrick Adem <161832897+jpa102@users.noreply.github.com>
2024-09-13 12:18:26 +0800

    operations.js の更新
    add new experiment flag code: return youtube dislike api - real time data update
    
    remove references to youtube (watch page html) at "revert the buttons to version 7" (also change the setTimeout value from 20 to 30)

--- faketube/2022/rsrc/js/operations.js
+++ faketube/2022/rsrc/js/operations.js
@@ -466,6 +466,14 @@ if (faketube.config_.EXPERIMENT_FLAGS.disable_dislike_counts == true) {
 	}, 1150);
 }
 
+// return youtube dislike api: real time data update
+if (faketube.config_.EXPERIMENT_FLAGS.return_youtube_dislike_api.enable_real_time_data == true) {
+	setTimeout(function() {
+		RefreshRydDataWithVideoId(ytVideoId);
+		document.querySelector(".video-metadata-renderer").innerText = totalviews + " views • " + ReceivedVideoMetadata.dateuploaded; // views and date
+	}, faketube.config_.EXPERIMENT_FLAGS.return_youtube_dislike_api.real_time_data_timer);
+}
+
 //	revert the buttons to version 7
 if (faketube.config_.EXPERIMENT_FLAGS.web_old_menu_buttons == true) {
 	setTimeout(function() {
@@ -585,19 +593,11 @@ if (faketube.config_.EXPERIMENT_FLAGS.web_old_menu_buttons == true) {
 						<div id="like" class="like-dislike-buttons version-7-buttons" onclick="likeButton()">
 							<button id="like-button" aria-pressed="false" title="I like this">
 								<div id="likebutton-icon" class="button-text-style" aria-hidden="true">
-									<yt-icon class="undefined" style="">
-										<!--css-build:shady-->
-										<!--css-build:shady-->
-										<yt-icon-shape class="style-scope yt-icon">
-											<icon-shape class="yt-spec-icon-shape">
-												<div style="width: 100%; height: 100%; fill: currentcolor;">
-													<svg xmlns="http://www.w3.org/2000/svg" enable-background="new 0 0 24 24" height="24" viewBox="0 0 24 24" width="24" focusable="false" style="pointer-events: none; display: block; width: 100%; height: 100%;">
-														<path d="M18.77,11h-4.23l1.52-4.94C16.38,5.03,15.54,4,14.38,4c-0.58,0-1.14,0.24-1.52,0.65L7,11H3v10h4h1h9.43 c1.06,0,1.98-0.67,2.19-1.61l1.34-6C21.23,12.15,20.18,11,18.77,11z M7,20H4v-8h3V20z M19.98,13.17l-1.34,6 C18.54,19.65,18.03,20,17.43,20H8v-8.61l5.6-6.06C13.79,5.12,14.08,5,14.38,5c0.26,0,0.5,0.11,0.63,0.3 c0.07,0.1,0.15,0.26,0.09,0.47l-1.52,4.94L13.18,12h1.35h4.23c0.41,0,0.8,0.17,1.03,0.46C19.92,12.61,20.05,12.86,19.98,13.17z"></path>
-													</svg>
-												</div>
-											</icon-shape>
-										</yt-icon-shape>
-									</yt-icon>
+									<div style="width: 100%; height: 100%; fill: currentcolor;">
+										<svg xmlns="http://www.w3.org/2000/svg" enable-background="new 0 0 24 24" height="24" viewBox="0 0 24 24" width="24" focusable="false" style="pointer-events: none; display: block; width: 100%; height: 100%;">
+											<path d="M18.77,11h-4.23l1.52-4.94C16.38,5.03,15.54,4,14.38,4c-0.58,0-1.14,0.24-1.52,0.65L7,11H3v10h4h1h9.43 c1.06,0,1.98-0.67,2.19-1.61l1.34-6C21.23,12.15,20.18,11,18.77,11z M7,20H4v-8h3V20z M19.98,13.17l-1.34,6 C18.54,19.65,18.03,20,17.43,20H8v-8.61l5.6-6.06C13.79,5.12,14.08,5,14.38,5c0.26,0,0.5,0.11,0.63,0.3 c0.07,0.1,0.15,0.26,0.09,0.47l-1.52,4.94L13.18,12h1.35h4.23c0.41,0,0.8,0.17,1.03,0.46C19.92,12.61,20.05,12.86,19.98,13.17z"></path>
+										</svg>
+									</div>
 								</div>
 							</button>
 							<span id="like-count-renderer" class="menu-buttons-text"></span>
@@ -606,19 +606,11 @@ if (faketube.config_.EXPERIMENT_FLAGS.web_old_menu_buttons == true) {
 						<div id="dislike" class="like-dislike-buttons version-7-buttons" onclick="dislikeButton()">
 							<button id="dislike-button" aria-pressed="false" title="I dislike this">
 								<div id="dislikebutton-icon" class="button-text-style" aria-hidden="true">
-									<yt-icon class="undefined" style="">
-										<!--css-build:shady-->
-										<!--css-build:shady-->
-										<div class="style-scope yt-icon">
-											<icon-shape class="yt-spec-icon-shape">
-												<div style="width: 100%; height: 100%; fill: currentcolor;">
-													<svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24" focusable="false" style="pointer-events: none; display: block; width: 100%; height: 100%;">
-														<path d="M17,4h-1H6.57C5.5,4,4.59,4.67,4.38,5.61l-1.34,6C2.77,12.85,3.82,14,5.23,14h4.23l-1.52,4.94C7.62,19.97,8.46,21,9.62,21 c0.58,0,1.14-0.24,1.52-0.65L17,14h4V4H17z M10.4,19.67C10.21,19.88,9.92,20,9.62,20c-0.26,0-0.5-0.11-0.63-0.3 c-0.07-0.1-0.15-0.26-0.09-0.47l1.52-4.94l0.4-1.29H9.46H5.23c-0.41,0-0.8-0.17-1.03-0.46c-0.12-0.15-0.25-0.4-0.18-0.72l1.34-6 C5.46,5.35,5.97,5,6.57,5H16v8.61L10.4,19.67z M20,13h-3V5h3V13z"></path>
-													</svg>
-												</div>
-											</icon-shape>
-										</div>
-									</yt-icon>
+									<div style="width: 100%; height: 100%; fill: currentcolor;">
+										<svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24" focusable="false" style="pointer-events: none; display: block; width: 100%; height: 100%;">
+											<path d="M17,4h-1H6.57C5.5,4,4.59,4.67,4.38,5.61l-1.34,6C2.77,12.85,3.82,14,5.23,14h4.23l-1.52,4.94C7.62,19.97,8.46,21,9.62,21 c0.58,0,1.14-0.24,1.52-0.65L17,14h4V4H17z M10.4,19.67C10.21,19.88,9.92,20,9.62,20c-0.26,0-0.5-0.11-0.63-0.3 c-0.07-0.1-0.15-0.26-0.09-0.47l1.52-4.94l0.4-1.29H9.46H5.23c-0.41,0-0.8-0.17-1.03-0.46c-0.12-0.15-0.25-0.4-0.18-0.72l1.34-6 C5.46,5.35,5.97,5,6.57,5H16v8.61L10.4,19.67z M20,13h-3V5h3V13z"></path>
+										</svg>
+									</div>
 								</div>
 							</button>
 							<span id="dislike-count-renderer" class="menu-buttons-text"></span>
@@ -643,17 +635,11 @@ if (faketube.config_.EXPERIMENT_FLAGS.web_old_menu_buttons == true) {
 					<div id="share" class="menu-buttons-actions version-7-buttons" onclick="shareButton()">
 						<button id="share-button" class="menu-buttons menu-buttons-text" title="Share this video">
 							<div id="sharebutton-icon">
-								<yt-icon class="undefined" style="width: 24px; height: 24px;">
-									<yt-icon-shape class="style-scope yt-icon">
-										<icon-shape class="yt-spec-icon-shape">
-											<div style="width: 100%; height: 100%; fill: currentcolor;">
-												<svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24" focusable="false" style="pointer-events: none; display: block; width: 100%; height: 100%;">
-													<path d="M15 5.63 20.66 12 15 18.37V14h-1c-3.96 0-7.14 1-9.75 3.09 1.84-4.07 5.11-6.4 9.89-7.1l.86-.13V5.63M14 3v6C6.22 10.13 3.11 15.33 2 21c2.78-3.97 6.44-6 12-6v6l8-9-8-9z"></path>
-												</svg>
-											</div>
-										</icon-shape>
-									</yt-icon-shape>
-								</yt-icon>
+								<div style="width: 100%; height: 100%; fill: currentcolor;">
+									<svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24" focusable="false" style="pointer-events: none; display: block; width: 100%; height: 100%;">
+										<path d="M15 5.63 20.66 12 15 18.37V14h-1c-3.96 0-7.14 1-9.75 3.09 1.84-4.07 5.11-6.4 9.89-7.1l.86-.13V5.63M14 3v6C6.22 10.13 3.11 15.33 2 21c2.78-3.97 6.44-6 12-6v6l8-9-8-9z"></path>
+									</svg>
+								</div>
 							</div>
 						</button>
 						<span id="share-text-renderer" class="menu-buttons-text">share</span>
@@ -661,17 +647,11 @@ if (faketube.config_.EXPERIMENT_FLAGS.web_old_menu_buttons == true) {
 					<div id="download" class="menu-buttons-actions version-7-buttons">
 						<button id="download-button" class="menu-buttons menu-buttons-text" title="Download this video">
 							<div id="downloadbutton-icon">
-								<yt-icon class="undefined" style="width: 24px; height: 24px;">
-									<yt-icon-shape class="style-scope yt-icon">
-										<icon-shape class="yt-spec-icon-shape">
-											<div style="width: 100%; height: 100%; fill: currentcolor;">
-												<svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24" focusable="false" style="pointer-events: none; display: block; width: 100%; height: 100%;">
-													<path d="M17 18v1H6v-1h11zm-.5-6.6-.7-.7-3.8 3.7V4h-1v10.4l-3.8-3.8-.7.7 5 5 5-4.9z"></path>
-												</svg>
-											</div>
-										</icon-shape>
-									</yt-icon-shape>
-								</yt-icon>
+								<div style="width: 100%; height: 100%; fill: currentcolor;">
+									<svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24" focusable="false" style="pointer-events: none; display: block; width: 100%; height: 100%;">
+										<path d="M17 18v1H6v-1h11zm-.5-6.6-.7-.7-3.8 3.7V4h-1v10.4l-3.8-3.8-.7.7 5 5 5-4.9z"></path>
+									</svg>
+								</div>
 							</div>
 						</button>
 						<span id="download-text-renderer" class="menu-buttons-text">Download</span>
@@ -679,34 +659,22 @@ if (faketube.config_.EXPERIMENT_FLAGS.web_old_menu_buttons == true) {
 					<div id="save" class="menu-buttons-actions version-7-buttons" onclick="saveButton()">
 						<button id="save-button" class="menu-buttons menu-buttons-text" title="Save this video">
 							<div id="savebutton-icon">
-								<yt-icon class="undefined style-scope ytd-menu-renderer" style="width: 24px; height: 24px;">
-									<yt-icon-shape class="style-scope yt-icon">
-										<icon-shape class="yt-spec-icon-shape">
-											<div style="width: 100%; height: 100%; fill: currentcolor;">
-												<svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24" focusable="false" style="pointer-events: none; display: block; width: 100%; height: 100%;">
-													<path d="M22 13h-4v4h-2v-4h-4v-2h4V7h2v4h4v2zm-8-6H2v1h12V7zM2 12h8v-1H2v1zm0 4h8v-1H2v1z"></path>
-												</svg>
-											</div>
-										</icon-shape>
-									</yt-icon-shape>
-								</yt-icon>
+								<div style="width: 100%; height: 100%; fill: currentcolor;">
+									<svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24" focusable="false" style="pointer-events: none; display: block; width: 100%; height: 100%;">
+										<path d="M22 13h-4v4h-2v-4h-4v-2h4V7h2v4h4v2zm-8-6H2v1h12V7zM2 12h8v-1H2v1zm0 4h8v-1H2v1z"></path>
+									</svg>
+								</div>
 							</div>
 						</button>
 						<span id="save-text-renderer" class="menu-buttons-text">save</span>
 					</div>
 					<button id="more-button" class="menu-buttons version-7-buttons menu-buttons-text" onclick="moreButton()" title="More actions">
 						<div id="morebutton-icon">
-							<yt-icon style="width: 24px; height: 24px;">
-								<yt-icon-shape class="style-scope yt-icon">
-									<icon-shape class="yt-spec-icon-shape">
-										<div style="width: 100%; height: 100%; fill: currentcolor;">
-											<svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24" focusable="false" style="pointer-events: none; display: block; width: 100%; height: 100%;">
-												<path d="M7.5 12c0 .83-.67 1.5-1.5 1.5s-1.5-.67-1.5-1.5.67-1.5 1.5-1.5 1.5.67 1.5 1.5zm4.5-1.5c-.83 0-1.5.67-1.5 1.5s.67 1.5 1.5 1.5 1.5-.67 1.5-1.5-.67-1.5-1.5-1.5zm6 0c-.83 0-1.5.67-1.5 1.5s.67 1.5 1.5 1.5 1.5-.67 1.5-1.5-.67-1.5-1.5-1.5z"></path>
-											</svg>
-										</div>
-									</icon-shape>
-								</yt-icon-shape>
-							</yt-icon>
+							<div style="width: 100%; height: 100%; fill: currentcolor;">
+								<svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24" focusable="false" style="pointer-events: none; display: block; width: 100%; height: 100%;">
+									<path d="M7.5 12c0 .83-.67 1.5-1.5 1.5s-1.5-.67-1.5-1.5.67-1.5 1.5-1.5 1.5.67 1.5 1.5zm4.5-1.5c-.83 0-1.5.67-1.5 1.5s.67 1.5 1.5 1.5 1.5-.67 1.5-1.5-.67-1.5-1.5-1.5zm6 0c-.83 0-1.5.67-1.5 1.5s.67 1.5 1.5 1.5 1.5-.67 1.5-1.5-.67-1.5-1.5-1.5z"></path>
+								</svg>
+							</div>
 						</div>
 					</button>
 				</div>
@@ -715,9 +683,9 @@ if (faketube.config_.EXPERIMENT_FLAGS.web_old_menu_buttons == true) {
 		);
 		document.querySelector("ratio-bar-renderer").hidden = false;
 		
-		if (document.querySelector("#like-count-renderer").innerText == "") document.querySelector("#like-count-renderer").innerText = formattedlikes;
-		if (document.querySelector("#dislike-count-renderer").innerText == "") document.querySelector("#dislike-count-renderer").innerText = formatteddislikes;
+		if (document.querySelector("#like-count-renderer").innerText == "") { document.querySelector("#like-count-renderer").innerText = formattedlikes; }
+		if (document.querySelector("#dislike-count-renderer").innerText == "") { document.querySelector("#dislike-count-renderer").innerText = formatteddislikes; }
 
 		document.querySelector("ratio-bar-renderer").style = "width: " + barwidth + "px";
-	}, 20);
+	}, 30);
 }
