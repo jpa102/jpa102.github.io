f869ddc61c4b3ccf5401500cf2c31dbe170de5ba
John Patrick Adem <161832897+jpa102@users.noreply.github.com>
2024-09-13 17:14:39 +0800

    faketube.js の更新
    update the return_youtube_dislike_api object
    
    also include comments in most experiment flags

--- faketube/2022/rsrc/js/faketube.js
+++ faketube/2022/rsrc/js/faketube.js
@@ -5,8 +5,8 @@
 */
 
 const faketube = {
-	apply_favorite_site_authors_background_color: false,
 	config_: {
+		apply_favorite_site_authors_background_color: false, // apply my favorite light theme color in the page
 		downloader_site_urls: {
 			"urls": [
 				{
@@ -19,19 +19,20 @@ const faketube = {
 		},
 		web_page_editable: false,
 		EXPERIMENT_FLAGS: {
-			apply_debug_metadata_immediately: true,
-			disable_dislike_counts: false,
-			display_dislike_pressed_popup: false,
+			apply_debug_metadata_immediately: true, // fetch and inject the description data (if available)
+			disable_dislike_counts: false, // don't display the dislike counts in the dislike button
+			display_dislike_pressed_popup: false, // display a popup every time you press the dislike button
 			return_youtube_dislike_api: {
-				enable_real_time_data: false,
-				real_time_data_timer: 60000
+				enable_real_time_data: false, // make the ryd data displayed on the page refresh itself
+				real_time_data_timer: 60000, // the time to refresh the ryd data displayed
+				ryd_request_loop_limit: 1000 // 10000 is the limit per day, 100 per minute
 			},
-			third_party_downloader_test: false,
-			video_player_non_rounded: false,
+			third_party_downloader_test: false, // turn on or off the ability to download the video from a another site
+			video_player_non_rounded: false, // make the iframe lose its border-radius (sharp edges look)
 			web_old_channel_info: false,
 			web_old_description: false,
-			web_old_menu_buttons: false,
-			web_simplified_action_buttons: false
+			web_old_menu_buttons: false, // make the action buttons below the video player like it's from 2021 layout
+			web_simplified_action_buttons: false // makes some action buttons look compact
 		}
 	}
 }
