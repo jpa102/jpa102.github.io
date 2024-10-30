f66e1afd839d120ea0218865dffeaeaf076a38ae
John Patrick Adem <161832897+jpa102@users.noreply.github.com>
2024-10-30 19:57:41 +0800

    Add files via upload

---
+++ activity_code_hehe/01 LMS Activity 1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdemCsharp
+{
+	class MyProgramCode
+	{
+		static void Main(string[] args)
+		{
+			const int OrigNum = 12;
+			
+			Console.WriteLine("[1] enter a number: ");
+			int numone = Console.ReadLine();
+			
+			Console.WriteLine("\n"); // new line
+			
+			Console.WriteLine("[2] enter a number: ");
+			int numtwo = Console.ReadLine();
+			
+			int total = numone + numtwo;
+			
+			if (total < OrigNum)
+			{
+				Console.WriteLine("the entered values don't match.\n\nExpected value: " + OrigNum + "\nTotal values: " + total);
+			}
+			else
+			{
+				Console.WriteLine("the entered values match!\n\nExpected value: " + OrigNum + "\nTotal values: " + total);
+			}
+		}
+	}
+}
+
+
