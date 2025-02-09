fea337e10120693b6a2eb5353dbe653fab4f0c15
John Patrick Adem <161832897+jpa102@users.noreply.github.com>
2025-02-09 23:26:42 +0800

    upload the tutorials folder

---
+++ blog/tutorials/rsrc/main.js
@@ -0,0 +1,10 @@
+/*
+	internal config
+*/
+let vibratems = 80; // time to vibrate
+
+// ==============================================================================
+
+function vibrate() {
+	navigator.vibrate(vibratems);
+}
