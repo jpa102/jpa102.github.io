ef44bc06947232eb6c4c4a5161985a1dc9ff8a73
John Patrick Adem <161832897+jpa102@users.noreply.github.com>
2025-02-09 14:33:35 +0800

    upload the beta 2022 layout

---
+++ faketube/2022/rsrc/js/setviewsanddate.js
@@ -0,0 +1,36 @@
+﻿/*
+
+	set views and date of the video published
+
+*/
+
+setTimeout(function(){
+	document.querySelector("#failed-to-load-views-and-date").style = "display: none";
+	document.querySelector("#video-views-and-date").insertAdjacentHTML(
+
+		"beforeend",
+
+		`
+
+		<p id="video-metadata" class="video-metadata-renderer">
+
+			${totalviews} views • ${uploaddate}
+
+		</p>
+		`
+	);
+
+	// including the code in replacing the video title because why not? :D
+	
+	document.querySelector("#video-title-failed.video-title-failed-to-load").style = "display: none";
+	document.querySelector("#video-title-and-date-published").insertAdjacentHTML(
+		"afterbegin",
+		`
+		<h1 id="video" class="title">
+
+			${ytVideoTitle}
+
+		</h1>
+		`
+	);
+}, 900);
