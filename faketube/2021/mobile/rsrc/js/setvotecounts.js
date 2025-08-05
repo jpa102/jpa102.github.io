8f4294835be55e989e2b0a650e965a7fea519036
John Patrick Adem <161832897+jpa102@users.noreply.github.com>
2025-08-05 22:26:04 +0800

    reupload the 2021 mobile layout (again)
    reupload the 2021 mobile layout (again)
    oopsie, uploaded at the root directory
    
    heavily refactored the variables used in noembed and ryd
    because it's refactored, it broke entirely
    
    changes:
        - some css improvements
        - use jsConsole.js with no 4 space chars as indents (reducing file size)
        - description page is now alive (although still unfinished)
        - instead of injecting the ifeame html, inject the embed link through src
        - debugging: replacing videos with id is now more reliable
        - introduce 2 new experiment flags (1 experiment flag fixed due to it being at the wrong place)
        - improve the fetching in ryd api (it was a mess, making multiple requests in some js files)

---
+++ faketube/2021/mobile/rsrc/js/setvotecounts.js
@@ -0,0 +1,21 @@
+/*
+	set the vote count fields (likes and dislikes)
+*/
+
+//	set the vote counts in the buttons
+setTimeout(function(){
+	// likes
+	document.querySelector("#like-count-renderer").innerText = numberFormat(global_data.ryd_data.likeCount);
+	
+	// dislikes
+	document.querySelector("#dislike-count-renderer").innerText = numberFormat(global_data.ryd_data.dislikeCount);
+}, __faketube_stlkedlkecnts);
+
+setTimeout(function() {
+	document.querySelector("#like-button").ariaLabel="Like this video along with " + global_data.ryd_data.LikesFromRYD + " other people";
+	document.querySelector("#dislike-button").ariaLabel="Dislike this video along with " + global_data.ryd_data.DislikesFromRYD + " other people";
+
+	// inject the values into like and dislike text renderers (description page)
+	document.querySelector("#total-like-counts-renderer").innerText = global_data.ryd_data.LikesFromRYD;
+	document.querySelector("#total-dislike-counts-renderer").innerText = global_data.ryd_data.DislikesFromRYD;
+}, __faketube_stvtecnts);
