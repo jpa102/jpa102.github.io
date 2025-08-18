8744e006cf6f1061c66fc9c8f09d7295f0f19df7
John Patrick Adem <161832897+jpa102@users.noreply.github.com>
2025-08-18 21:50:57 +0800

    reupload the 2023 mobile layout yet again
    with updated code from last week, of course

---
+++ faketube/2023/mobile/rsrc/js/buttonslistener.js
@@ -0,0 +1,127 @@
+/*
+	==========================================================
+			Like and Dislike buttons
+	==========================================================
+*/
+function likeButton() {
+	likebuttonpressedcheck();
+}
+
+function dislikeButton() {
+	dislikebuttonpressedcheck();
+}
+
+
+
+/*
+	==========================================================
+				Other buttons
+	==========================================================
+*/
+function shareButton() {
+	console.log("function not implemented yet. [ln:21 buttonlistener.js]");
+}
+
+function liveChatButton() {
+	console.log("function not implemented yet. [ln:25 buttonlistener.js]");
+}
+
+function thanksButton() {
+	console.log("function not implemented yet. [ln:29 buttonlistener.js]");
+}
+
+function createButton() {
+	console.log("function not implemented yet. [ln:33 buttonlistener.js]");
+}
+
+function downloadButton() {
+	if (document.querySelector("#download-button").ariaPressed == "false") {
+		document.querySelector("#downloadbutton-icon > div > svg").remove();
+
+		document.querySelector("#downloadbutton-icon > div").insertAdjacentHTML(
+			"afterbegin",
+			`
+				<svg xmlns="http://www.w3.org/2000/svg" enable-background="new 0 0 24 24" height="24" viewBox="0 0 24 24" width="24" focusable="false" aria-hidden="true" style="pointer-events: none; display: inherit; width: 100%; height: 100%;">
+					<path d="M12,2C6.5,2,2,6.5,2,12s4.5,10,10,10s10-4.5,10-10S17.5,2,12,2z M17,18H7v-2h10V18z M10.3,14L7,10.7l1.4-1.4l1.9,1.9 l5.3-5.3L17,7.3L10.3,14z"></path>
+				</svg>
+			`
+		);
+
+		document.querySelector("#download-text-renderer").innerText = "Downloaded";
+		document.querySelector("#download-button").ariaPressed = "true";
+
+		setTimeout(function() {
+			alert("Download unavailable");
+		}, 900);
+		console.log("sorry, no video download available.\nin fact, there's not even a single line of code here that provides download functionality.  [ln:37 buttonlistener.js]");
+
+
+		return;
+	}
+
+	if (document.querySelector("#download-button").ariaPressed == "true") {
+		document.querySelector("#downloadbutton-icon > div > svg").remove();
+
+		document.querySelector("#downloadbutton-icon > div").insertAdjacentHTML(
+			"afterbegin",
+			`
+				<svg xmlns="http://www.w3.org/2000/svg" enable-background="new 0 0 24 24" height="24" viewBox="0 0 24 24" width="24" focusable="false" aria-hidden="true" style="pointer-events: none; display: inherit; width: 100%; height: 100%;">
+					<path d="M17 18v1H6v-1h11zm-.5-6.6-.7-.7-3.8 3.7V4h-1v10.4l-3.8-3.8-.7.7 5 5 5-4.9z"></path>
+				</svg>
+			`
+		);
+
+		document.querySelector("#download-text-renderer").innerText = "Download";
+		document.querySelector("#download-button").ariaPressed = "false";
+		return;
+	}
+}
+
+function clipButton() {
+	console.log("function not implemented yet. [ln:41 buttonlistener.js]");
+}
+
+function saveButton() {
+	if (document.querySelector("#save-button").ariaPressed == "false") {
+		document.querySelector("#savebutton-icon > div > svg").remove();
+
+		document.querySelector("#savebutton-icon > div").insertAdjacentHTML(
+			"afterbegin",
+			`
+				<svg xmlns="http://www.w3.org/2000/svg" enable-background="new 0 0 24 24" height="24" viewBox="0 0 24 24" width="24" focusable="false" aria-hidden="true" style="pointer-events: none; display: inherit; width: 100%; height: 100%;">
+					<path d="M4 20h14v1H3V6h1v14zM21 3v15H6V3h15zm-3.42 5.41L16.17 7l-4.18 4.18-1.58-1.58L9 11.01 11.99 14l5.59-5.59z"></path>
+
+				</svg>
+			`
+		);
+
+		document.querySelector("#save-text-renderer").innerText = "Saved";
+		document.querySelector("#save-button").ariaPressed = "true";
+		return;
+	}
+
+	if (document.querySelector("#save-button").ariaPressed == "true") {
+		document.querySelector("#savebutton-icon > div > svg").remove();
+
+		document.querySelector("#savebutton-icon > div").insertAdjacentHTML(
+			"afterbegin",
+			`
+				<svg xmlns="http://www.w3.org/2000/svg" enable-background="new 0 0 24 24" height="24" viewBox="0 0 24 24" width="24" focusable="false" aria-hidden="true" style="pointer-events: none; display: inherit; width: 100%; height: 100%;">
+					<path d="M4 20h14v1H3V6h1v14zm14-10h-4V6h-1v4H9v1h4v4h1v-4h4v-1zm3-7v15H6V3h15zm-1 1H7v13h13V4z"></path>
+				</svg>
+			`
+		);
+
+		document.querySelector("#save-text-renderer").innerText = "Save";
+		document.querySelector("#save-button").ariaPressed = "false";
+		return;
+	}
+}
+
+function reportButton() {
+	console.log("function not implemented yet. [ln:81 buttonlistener.js]");
+}
+
+function moreButton() {
+	console.log("function not implemented yet. [ln:85 buttonlistener.js]");
+}
---
+++ faketube/2023/mobile/rsrc/js/theme.js
@@ -0,0 +1,808 @@
+﻿/*
+	theme.js
+	
+	a javascript file that changes the page's theme
+	currently, there are three options:
+	
+		black hole
+		youtube's dark mode
+		2021 youtube dark mode
+		dark mode from devtools (chrome v109.0.5414.120)
+	
+	internal choices are: blackhole, darkmode, legacydarkmode, devtools_dark
+*/
+
+
+
+function _check() {
+	if (document.querySelector("#blackhole-theme") != null) {
+		document.querySelector("#blackhole-theme").remove();
+	}
+
+	if (document.querySelector("#dark-mode-theme") != null) {
+		document.querySelector("#dark-mode-theme").remove();
+	}
+
+	if (document.querySelector("#legacy-dark-mode-theme") != null) {
+		document.querySelector("#legacy-dark-mode-theme").remove();
+	}
+
+	if (document.querySelector("#devtools-dark-theme") != null) {
+		document.querySelector("#devtools-dark-theme").remove();
+	}
+}
+
+function changeThemeDisplay(colorOption) {
+	if (colorOption == null) { console.log("expected a value, got " + colorOption + " instead"); return; }
+
+	if (colorOption == "unset") { _check(); return; }
+
+	if (colorOption == "blackhole" && document.querySelector("#blackhole-theme") == null) {
+		_check();
+		document.querySelector("head").insertAdjacentHTML(
+			"beforeend",
+			`
+				<style id="blackhole-theme" text="text/css">
+					:root {
+						--main-background-color: black;
+						--white-color: white;
+						--gray-color: #adadad;
+					}
+
+					body {
+						background: var(--main-background-color) !important;
+					}
+
+					#topbar {
+						background: var(--main-bankground-color) !important;
+						border-bottom: 1px solid rgb(48, 48, 48);
+					}
+
+					#app-title-text {
+						color: var(--white-color);
+					}
+
+					.sidenav {
+						background: #161616 !important;
+					}
+
+					.sidenav a:hover {
+						background: #262626 !important;
+						color: var(--gray-color) !important;
+					}
+
+					.sidenav a:active {
+						background: rgba(30, 124, 255, 0.15) !important;
+						color: dodgerblue !important;
+					}
+
+					.yt-icon {
+						fill: var(--white-color);
+					}
+
+					#video-metadata.video-metadata-renderer {
+						color: var(--gray-color) !important;
+					}
+
+					#video.title {
+						font-weight: 500;
+						color: var(--white-color);
+					}
+
+					#statistics {
+						color: var(--white-color) !important;
+					}
+
+					.vote-count {
+						color: var(--gray-color) !important;
+					}
+
+					#video-average-rating {
+						color: var(--gray-color) !important;
+					}
+
+					#like-bar {
+						background: var(--white-color) !important;
+					}
+
+					#yt-channel-name-link {
+						color: var(--white-color) !important;
+					}
+
+					a.yt-channel-sub-count {
+						color: var(--gray-color) !important;
+					}
+
+					#subscribe-text-link {
+						color: black;
+						background: white;
+					}
+
+					#comment-field {
+						background-color: rgba(255, 255, 255, 0.1);
+					}
+
+					.menu-buttons {
+						background-color: rgba(255, 255, 255, 0.1) !important;
+					}
+
+					.menu-buttons-text, .button-text-style {
+						color: var(--white-color) !important;
+					}
+
+					.menu-buttons:active {
+						border: 1px solid rgb(64, 64, 64) !important;
+						background-color: rgb(47, 47, 47) !important;
+						transition: .2s cubic-bezier(0.18, 0.89, 0.32, 1.28) !important;
+					}
+
+					#like-button:active {
+						background-color: rgba(255, 255, 255, 0.3) !important;
+					}
+
+					.button-border {
+						border-left: 1px solid rgba(255, 255, 255, 0.1) !important;
+					}
+
+					#dislike-button:active {
+						background-color: rgba(255, 255, 255, 0.3) !important;
+					}
+
+					.menu-buttons:active {
+						background-color: rgba(255, 255, 255, 0.3) !important;
+						transition: .2s cubic-bezier(0.18, 0.89, 0.32, 1.28);
+					}
+
+					#display-sec-desc-button-icon,
+					#hide-sec-desc-button-icon {
+						fill: var(--white-color) !important;
+					}
+
+					#description-title {
+						color: var(--white-color) !important;
+					}
+
+					#description-text {
+						color: var(--gray-color) !important;
+						background: rgba(255, 255, 255, 0.1);
+					}
+
+					#border-top-metadata,
+					#border-bottom-metadata {
+						border-bottom: 1px solid rgba(255, 255, 255, 0.1) !important;
+					}
+
+					#comment-section {
+						background: rgba(255, 255, 255, 0.1) !important;
+					}
+
+					#comment-caption {
+						color: var(--white-color) !important;
+					}
+
+					#comment-counts {
+						color: var(--gray-color) !important;
+					}
+
+					input {
+						color: var(--white-color) !important;
+					}
+
+					#video-title-desc {
+						color: var(--white-color);
+						font-weight: 500;
+					}
+
+					#yt-channel-name-link-desc {
+						color: var(--white-color) !important;
+					}
+
+					#video-stats-desc {
+						color: var(--white-color);
+						font-weight: 500;
+					}
+
+					#more-button {
+						color: var(--white-color);
+					}
+
+					#thick-border {
+						border: 4px solid rgba(255, 255, 255, 0.1);
+					}
+
+					.loading-video-thumbnails,
+					.img-author-skeleton,
+					.video-title-skeleton,
+					.video-author-skeleton,
+					.shorts-skeleton {
+						background: rgba(255, 255, 255, 0.1);
+					}
+				</style>
+			`
+		);
+
+		return;
+	} else {
+		console.log("the blackhole theme is already applied");
+		DebugJS.console.log("info", "the blackhole theme is already applied");
+	}
+
+	if (colorOption == "darkmode" && document.querySelector("#dark-mode-theme") == null) {
+		_check();
+		document.querySelector("head").insertAdjacentHTML(
+			"beforeend",
+			`
+				<style id="dark-mode-theme" type="text/css">
+					:root {
+						--main-background-color: #0f0f0f;
+						--white-color: white;
+						--gray-color: #adadad;
+					}
+
+					body {
+						background: var(--main-background-color) !important;
+					}
+
+					#topbar {
+						background: var(--main-bankground-color) !important;
+						border-bottom: 1px solid rgb(48, 48, 48);
+					}
+
+					#app-title-text {
+						color: var(--white-color);
+					}
+
+					.sidenav {
+						background: #161616 !important;
+					}
+
+					.sidenav a:hover {
+						background: #262626 !important;
+						color: var(--gray-color) !important;
+					}
+
+					.sidenav a:active {
+						background: rgba(30, 124, 255, 0.15) !important;
+						color: dodgerblue !important;
+					}
+
+					.yt-icon {
+						fill: var(--white-color);
+					}
+
+					#video-metadata.video-metadata-renderer {
+						color: var(--gray-color) !important;
+					}
+
+					#video.title {
+						font-weight: 500;
+						color: var(--white-color);
+					}
+
+					#statistics {
+						color: var(--white-color) !important;
+					}
+
+					.vote-count {
+						color: var(--gray-color) !important;
+					}
+
+					#video-average-rating {
+						color: var(--gray-color) !important;
+					}
+
+					#like-bar {
+						background: var(--white-color) !important;
+					}
+
+					#yt-channel-name-link {
+						color: var(--white-color) !important;
+					}
+
+					a.yt-channel-sub-count {
+						color: var(--gray-color) !important;
+					}
+
+					#subscribe-text-link {
+						color: black;
+						background: white;
+					}
+
+					#comment-field {
+						background-color: rgba(255, 255, 255, 0.1);
+					}
+
+					.menu-buttons {
+						background-color: rgba(255, 255, 255, 0.1) !important;
+					}
+
+					.menu-buttons-text, .button-text-style {
+						color: var(--white-color) !important;
+					}
+
+					.menu-buttons:active {
+						border: 1px solid rgb(64, 64, 64) !important;
+						background-color: rgb(47, 47, 47) !important;
+						transition: .2s cubic-bezier(0.18, 0.89, 0.32, 1.28) !important;
+					}
+
+					#like-button:active {
+						background-color: rgba(255, 255, 255, 0.3) !important;
+					}
+
+					.button-border {
+						border-left: 1px solid rgba(255, 255, 255, 0.1) !important;
+					}
+
+					#dislike-button:active {
+						background-color: rgba(255, 255, 255, 0.3) !important;
+					}
+
+					.menu-buttons:active {
+						background-color: rgba(255, 255, 255, 0.3) !important;
+						transition: .2s cubic-bezier(0.18, 0.89, 0.32, 1.28);
+					}
+
+					#display-sec-desc-button-icon,
+					#hide-sec-desc-button-icon {
+						fill: var(--white-color) !important;
+					}
+
+					#description-title {
+						color: var(--white-color) !important;
+					}
+
+					#description-text {
+						color: var(--gray-color) !important;
+						background: rgba(255, 255, 255, 0.1);
+					}
+
+					#border-top-metadata,
+					#border-bottom-metadata {
+						border-bottom: 1px solid rgba(255, 255, 255, 0.1) !important;
+					}
+
+					#comment-section {
+						background: rgba(255, 255, 255, 0.1) !important;
+					}
+
+					#comment-caption {
+						color: var(--white-color) !important;
+					}
+
+					#comment-counts {
+						color: var(--gray-color) !important;
+					}
+
+					input {
+						color: var(--white-color) !important;
+					}
+
+					#video-title-desc {
+						color: var(--white-color);
+						font-weight: 500;
+					}
+
+					#yt-channel-name-link-desc {
+						color: var(--white-color) !important;
+					}
+
+					#video-stats-desc {
+						color: var(--white-color);
+						font-weight: 500;
+					}
+
+					#more-button {
+						color: var(--white-color);
+					}
+
+					#thick-border {
+						border: 4px solid rgba(255, 255, 255, 0.1);
+					}
+
+					.loading-video-thumbnails,
+					.img-author-skeleton,
+					.video-title-skeleton,
+					.video-author-skeleton,
+					.shorts-skeleton {
+						background: rgba(255, 255, 255, 0.1);
+					}
+				</style>
+			`
+		);
+
+		return;
+	} else {
+		console.log("the dark mode theme is already applied");
+		DebugJS.console.log("info", "the dark mode theme is already applied");
+	}
+	
+	if (colorOption == "legacydarkmode" && document.querySelector("#legacy-dark-mode-theme") == null) {
+		_check();
+		document.querySelector("head").insertAdjacentHTML(
+			"beforeend",
+			`
+				<style id="legacy-dark-mode-theme" type="text/css">
+					:root {
+						--main-background-color: #212121;
+						--white-color: white;
+						--gray-color: #adadad;
+					}
+
+					body {
+						background: var(--main-background-color) !important;
+					}
+
+					#topbar {
+						background: var(--main-bankground-color) !important;
+						border-bottom: 1px solid rgb(48, 48, 48);
+					}
+
+					#app-title-text {
+						color: var(--white-color);
+					}
+
+					.sidenav {
+						background: #161616 !important;
+					}
+
+					.sidenav a:hover {
+						background: #262626 !important;
+						color: var(--gray-color) !important;
+					}
+
+					.sidenav a:active {
+						background: rgba(30, 124, 255, 0.15) !important;
+						color: dodgerblue !important;
+					}
+
+					.yt-icon {
+						fill: var(--white-color);
+					}
+
+					#video-metadata.video-metadata-renderer {
+						color: var(--gray-color) !important;
+					}
+
+					#video.title {
+						font-weight: 500;
+						color: var(--white-color);
+					}
+
+					#statistics {
+						color: var(--white-color) !important;
+					}
+
+					.vote-count {
+						color: var(--gray-color) !important;
+					}
+
+					#video-average-rating {
+						color: var(--gray-color) !important;
+					}
+
+					#like-bar {
+						background: var(--white-color) !important;
+					}
+
+					#yt-channel-name-link {
+						color: var(--white-color) !important;
+					}
+
+					a.yt-channel-sub-count {
+						color: var(--gray-color) !important;
+					}
+
+					#subscribe-text-link {
+						color: black;
+						background: white;
+					}
+
+					#comment-field {
+						background-color: rgba(255, 255, 255, 0.1);
+					}
+
+					.menu-buttons {
+						background-color: rgba(255, 255, 255, 0.1) !important;
+					}
+
+					.menu-buttons-text, .button-text-style {
+						color: var(--white-color) !important;
+					}
+
+					.menu-buttons:active {
+						border: 1px solid rgb(64, 64, 64) !important;
+						background-color: rgb(47, 47, 47) !important;
+						transition: .2s cubic-bezier(0.18, 0.89, 0.32, 1.28) !important;
+					}
+
+					#like-button:active {
+						background-color: rgba(255, 255, 255, 0.3) !important;
+					}
+
+					.button-border {
+						border-left: 1px solid rgba(255, 255, 255, 0.1) !important;
+					}
+
+					#dislike-button:active {
+						background-color: rgba(255, 255, 255, 0.3) !important;
+					}
+
+					.menu-buttons:active {
+						background-color: rgba(255, 255, 255, 0.3) !important;
+						transition: .2s cubic-bezier(0.18, 0.89, 0.32, 1.28);
+					}
+
+					#display-sec-desc-button-icon,
+					#hide-sec-desc-button-icon {
+						fill: var(--white-color) !important;
+					}
+
+					#description-title {
+						color: var(--white-color) !important;
+					}
+
+					#description-text {
+						color: var(--gray-color) !important;
+						background: rgba(255, 255, 255, 0.1);
+					}
+
+					#border-top-metadata,
+					#border-bottom-metadata {
+						border-bottom: 1px solid rgba(255, 255, 255, 0.1) !important;
+					}
+
+					#comment-section {
+						background: rgba(255, 255, 255, 0.1) !important;
+					}
+
+					#comment-caption {
+						color: var(--white-color) !important;
+					}
+
+					#comment-counts {
+						color: var(--gray-color) !important;
+					}
+
+					input {
+						color: var(--white-color) !important;
+					}
+
+					#video-title-desc {
+						color: var(--white-color);
+						font-weight: 500;
+					}
+
+					#yt-channel-name-link-desc {
+						color: var(--white-color) !important;
+					}
+
+					#video-stats-desc {
+						color: var(--white-color);
+						font-weight: 500;
+					}
+
+					#more-button {
+						color: var(--white-color);
+					}
+
+					#thick-border {
+						border: 4px solid rgba(255, 255, 255, 0.1);
+					}
+
+					.loading-video-thumbnails,
+					.img-author-skeleton,
+					.video-title-skeleton,
+					.video-author-skeleton,
+					.shorts-skeleton {
+						background: rgba(255, 255, 255, 0.1);
+					}
+				</style>
+			`
+		);
+
+		return;
+	} else {
+		console.log("the legacy dark mode theme is already applied");
+		DebugJS.console.log("info", "the legacy dark mode theme is already applied");
+	}
+
+	if (colorOption == "devtools_dark" && document.querySelector("#devtools-dark-theme") == null) {
+		_check();
+		document.querySelector("head").insertAdjacentHTML(
+			"beforeend",
+			`
+				<style id="devtools-dark-theme" text="text/css">
+					:root {
+						--main-background-color: rgb(32, 32, 35);
+						--white-color: white;
+						--gray-color: #adadad;
+					}
+
+					body {
+						background: var(--main-background-color) !important;
+					}
+
+					#topbar {
+						background: var(--main-bankground-color) !important;
+						border-bottom: 1px solid rgb(48, 48, 48);
+					}
+
+					#app-title-text {
+						color: var(--white-color);
+					}
+
+					.sidenav {
+						background: #161616 !important;
+					}
+
+					.sidenav a:hover {
+						background: #262626 !important;
+						color: var(--gray-color) !important;
+					}
+
+					.sidenav a:active {
+						background: rgba(30, 124, 255, 0.15) !important;
+						color: dodgerblue !important;
+					}
+
+					.yt-icon {
+						fill: var(--white-color);
+					}
+
+					#video-metadata.video-metadata-renderer {
+						color: var(--gray-color) !important;
+					}
+
+					#video.title {
+						font-weight: 500;
+						color: var(--white-color);
+					}
+
+					#statistics {
+						color: var(--white-color) !important;
+					}
+
+					.vote-count {
+						color: var(--gray-color) !important;
+					}
+
+					#video-average-rating {
+						color: var(--gray-color) !important;
+					}
+
+					#like-bar {
+						background: var(--white-color) !important;
+					}
+
+					#yt-channel-name-link {
+						color: var(--white-color) !important;
+					}
+
+					a.yt-channel-sub-count {
+						color: var(--gray-color) !important;
+					}
+
+					#subscribe-text-link {
+						color: black;
+						background: white;
+					}
+
+					#comment-field {
+						background-color: rgba(255, 255, 255, 0.1);
+					}
+
+					.menu-buttons {
+						background-color: rgba(255, 255, 255, 0.1) !important;
+					}
+
+					.menu-buttons-text, .button-text-style {
+						color: var(--white-color) !important;
+					}
+
+					.menu-buttons:active {
+						border: 1px solid rgb(64, 64, 64) !important;
+						background-color: rgb(47, 47, 47) !important;
+						transition: .2s cubic-bezier(0.18, 0.89, 0.32, 1.28) !important;
+					}
+
+					#like-button:active {
+						background-color: rgba(255, 255, 255, 0.3) !important;
+					}
+
+					.button-border {
+						border-left: 1px solid rgba(255, 255, 255, 0.1) !important;
+					}
+
+					#dislike-button:active {
+						background-color: rgba(255, 255, 255, 0.3) !important;
+					}
+
+					.menu-buttons:active {
+						background-color: rgba(255, 255, 255, 0.3) !important;
+						transition: .2s cubic-bezier(0.18, 0.89, 0.32, 1.28);
+					}
+
+					#display-sec-desc-button-icon,
+					#hide-sec-desc-button-icon {
+						fill: var(--white-color) !important;
+					}
+
+					#description-title {
+						color: var(--white-color) !important;
+					}
+
+					#description-text {
+						color: var(--gray-color) !important;
+						background: rgba(255, 255, 255, 0.1);
+					}
+
+					#border-top-metadata,
+					#border-bottom-metadata {
+						border-bottom: 1px solid rgba(255, 255, 255, 0.1) !important;
+					}
+
+					#comment-section {
+						background: rgba(255, 255, 255, 0.1) !important;
+					}
+
+					#comment-caption {
+						color: var(--white-color) !important;
+					}
+
+					#comment-counts {
+						color: var(--gray-color) !important;
+					}
+
+					input {
+						color: var(--white-color) !important;
+					}
+
+					#video-title-desc {
+						color: var(--white-color);
+						font-weight: 500;
+					}
+
+					#yt-channel-name-link-desc {
+						color: var(--white-color) !important;
+					}
+
+					#video-stats-desc {
+						color: var(--white-color);
+						font-weight: 500;
+					}
+
+					#more-button {
+						color: var(--white-color);
+					}
+
+					#thick-border {
+						border: 4px solid rgba(255, 255, 255, 0.1);
+					}
+
+					.loading-video-thumbnails,
+					.img-author-skeleton,
+					.video-title-skeleton,
+					.video-author-skeleton,
+					.shorts-skeleton {
+						background: rgba(255, 255, 255, 0.1);
+					}
+				</style>
+			`
+		);
+
+		return;
+	} else {
+		console.log("the devtools_dark theme is already applied");
+		DebugJS.console.log("info", "the devtools_dark theme is already applied");
+	}
+
+	if (colorOption == "lightmode") {
+		_check();
+
+		console.log("not implemented yet, sorry!");
+		DebugJS.console.log("warning", "not implemented yet, sorry!");
+
+		return;
+	}
+}
