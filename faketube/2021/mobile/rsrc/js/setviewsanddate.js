ffc8c0cb558433168e903f6dac1d03583002b6f1
John Patrick Adem <161832897+jpa102@users.noreply.github.com>
2025-12-03 01:19:15 +0800

    setviewsanddate.js の更新
    resolve some "is not defined" errors because it tries to directly get data from ytInitialData

--- faketube/2021/mobile/rsrc/js/setviewsanddate.js
+++ faketube/2021/mobile/rsrc/js/setviewsanddate.js
@@ -4,7 +4,7 @@
 //	set views and date of the video published
 
 setTimeout(function(){
-	document.querySelector("#video-metadata.video-metadata-renderer").innerText = ytInitialData.contents.twoColumnWatchNextResults.results.results.contents[0].videoPrimaryInfoRenderer.viewCount.videoViewCountRenderer.shortViewCount.simpleText + " • " + ytInitialData.contents.twoColumnWatchNextResults.results.results.contents[0].videoPrimaryInfoRenderer.relativeDateText.simpleText;
+	document.querySelector("#video-metadata.video-metadata-renderer").innerText = global_data.yt.viewCountFormatted + " • " + global_data.yt.uploadedYearsAgo;
 
 	// including the code in injecting the video title because why not? :D
 	document.querySelector("#video.title").innerText = global_data.yt.videoTitle;
@@ -17,5 +17,6 @@ setTimeout(function(){
 	document.querySelector("#total-view-counts-renderer").innerText = global_data.yt.viewCount.toLocaleString();
 
 	document.querySelector("#upload-date-renderer").innerText = global_data.yt.uploadYear;
+
 	document.querySelector("#upload-date-title").innerText = global_data.yt.uploadMonthDay;
 }, __faketube_stvwsndte);
