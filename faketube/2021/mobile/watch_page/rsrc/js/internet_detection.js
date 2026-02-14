1f55918cae5a229dcb044a08f0256b40a7885bfa
John Patrick Adem <161832897+jpa102@users.noreply.github.com>
2026-02-15 03:00:50 +0800

    Enhance internet detection with data attribute
    Added data attribute to HTML for online/offline status.

--- faketube/2021/mobile/watch_page/rsrc/js/internet_detection.js
+++ faketube/2021/mobile/watch_page/rsrc/js/internet_detection.js
@@ -6,15 +6,16 @@
 */
 
 window.addEventListener('online', (event) => {
+	document.querySelector("html").setAttribute("data-online-status", "online");
 	console.log(global_data._watch_page_strings._stored_vars.back_online_text_inject);
 	document.querySelector("#status-text").innerText = global_data._watch_page_strings._stored_vars.back_online_text_inject;
 	setTimeout(() => {
 		document.querySelector("#internet-status-bottom").style = "transform: translateY(32px);";
-		document.querySelector("#status-text").style = "background: #2ba640";
 	}, __faketube_wtchpge_intrnt_btm_notif_clpse_ms);
 });
 
 window.addEventListener('offline', (event) => {
+	document.querySelector("html").setAttribute("data-online-status", "offline");
 	console.log(global_data._watch_page_strings._stored_vars.no_internet_connection_text_inject);
 	document.querySelector("#status-text").innerText = global_data._watch_page_strings._stored_vars.no_internet_connection_text_inject;
 	setTimeout(() => {
