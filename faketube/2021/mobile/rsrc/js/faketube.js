77b76cc3bb24d3b8173272e900ce8e9a05b5a4de
John Patrick Adem <161832897+jpa102@users.noreply.github.com>
2026-03-10 11:40:53 +0800

    Add initial implementation of faketube.js

---
+++ faketube/2021/mobile/rsrc/js/faketube.js
@@ -0,0 +1,118 @@
+/*
+	faketube.js
+	
+	a file where you can freely configure it at devtools (breakpoint)
+	ONLY the owner can change configs "server-side" at any time (jpa102)
+
+	known for hosting the experiment flags which the user can freely try out (assuming it doesn't break XD)
+*/
+
+var faketube = {
+	config_: {
+		apply_favorite_site_authors_background_color: false, // apply my favorite light theme color in the page
+		display_intfuncs_button: false, // display the 'intfuncs' (internal functions) button in the action bar at watch page
+		unhide_gradient_header: false, // this header is for mimicking the m.youtube.com style, it will be hidden for now because things like search aren't implemented yet
+		web_download_button_hidden: false, // hide the download button in the actions container
+		web_page_editable: false, // no need to do the document.designMode trick in devtools
+		web_stop_ads_button_hidden: false, // hide the stop ads button
+		EXPERIMENT_FLAGS: {
+			forced_country_code_and_language: "", // input a country code and language (example: ja-JP for japanese - japan)
+			match_older_15_xx_xx_version: false, // inject and style the page from 15.xx.xx versions of youtube app
+			match_older_16_xx_xx_version: false // inject and style the page from older 16.xx.xx versions of youtube app
+		}
+	}
+} // set a breakpoint here if you want to change flags client-side, then go to the console tab
+
+
+
+var com = {
+	faketube: {
+		web: {
+			debug: {
+				displayhtml() {
+					document.querySelector("html").hidden = false;
+				},
+				displaynonehtml() {
+					document.querySelector("html").hidden = true;
+				},
+				getbrowserlocale() {
+					return navigator.language;
+				},
+				gethtmllocale() {
+					if (document.querySelector("html").lang != "") {
+						return document.querySelector("html").lang;
+					} else {
+						return "no html language set";
+					}
+				},
+				getcurrentscreensizepx() {
+					let scr_x = document.querySelector("html").clientWidth;
+					let scr_y = document.querySelector("html").clientHeight;
+					return `screen size in px: ${scr_x}px, ${scr_y}px`;
+				},
+				sethtmllocale(language) {
+					if (language != "") {
+						document.querySelector("html").lang = language;
+					} else {
+						console.log(`you must set a locale like ja-JP (string type)`);
+					}
+				}
+			},
+			// this is just a wrapper to display.theme object
+			display(themeIndex) {// clear out the theme setting
+				if (themeIndex == 0) {
+					display.theme._clear();
+				}
+				// darkmode theme
+				if (themeIndex == 1) {
+					display.theme.darkmode();
+				}
+				// darkmode theme
+				if (themeIndex == 2) {
+					display.theme.darkerdarkmode();
+				}
+				// black hole theme
+				if (themeIndex == 3) {
+					display.theme.blackhole();
+				}
+				// if for whatever reason, is called without a value
+				if (themeIndex == undefined || themeIndex == "" || themeIndex == null) {
+					console.log(`please provide an index number\n\n\t0 - clear out the theme\n\t1 - dark mode\n\t2 - darker dark mode\n\t3 - black hole`);
+				}
+				
+			},
+			localStorage_storeVideoId(videoId) {
+				if (videoId == "" || videoId == undefined || videoId == null) {
+					console.log(`there's no value provided\nvideoId: ${videoId}`);
+					return;
+				}
+
+				if (videoId.length < 11 || videoId.length > 11) {
+					console.log(`the video id provided is not exactly 11 characters\nlength: ${videoId.length}`);
+					return;
+				}
+
+				// ===== the main execution part =====
+
+				// check if the videoids key doesn't exist yet, then create it
+				if (localStorage.videoids === undefined) {
+					localStorage.setItem("videoids", JSON.stringify([]));
+				}
+
+
+				// perform a check if the video id is already inside the array to avoid duplicates
+				let v = JSON.parse(localStorage.videoids);
+
+				for (let i = 0; i < v.length; i++) {
+					if (v[i].includes(videoId) == true) {
+						return; // terminating because the video id is already present in the array
+					}
+				}
+
+				// actually store a video id in the array
+				v.push(videoId);
+				localStorage.setItem("videoids", JSON.stringify(v));
+			}
+		}
+	}
+}
