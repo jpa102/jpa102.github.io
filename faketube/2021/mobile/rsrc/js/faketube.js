288cf7f4325f6ffcacd3052f2bb0d8c368dce2d3
John Patrick Adem <161832897+jpa102@users.noreply.github.com>
2026-03-21 08:17:10 +0800

    faketube.js の更新

--- faketube/2021/mobile/rsrc/js/faketube.js
+++ faketube/2021/mobile/rsrc/js/faketube.js
@@ -10,6 +10,7 @@
 var faketube = {
 	config_: {
 		apply_favorite_site_authors_background_color: false, // apply my favorite light theme color in the page
+		debug_logging: false, // option to either enable or disable debug logging
 		display_intfuncs_button: false, // display the 'intfuncs' (internal functions) button
 		web_page_editable: false, // no need to do the document.designMode trick in devtools
 		web_stop_ads_button_hidden: false, // hide the stop ads button
@@ -35,6 +36,10 @@ var com = {
 				displaynonehtml() {
 					document.querySelector("html").hidden = true;
 				},
+				// this is just a wrapper to plain displaySavedVideoIds() function
+				displaySavedVideoIds() {
+					displaySavedVideoIds();
+				},
 				getbrowserlocale() {
 					return navigator.language;
 				},
@@ -56,30 +61,71 @@ var com = {
 					} else {
 						console.log(`you must set a locale like ja-JP (string type)`);
 					}
+				},
+				ReplaceTextInHtmlTitleTag(inputtext) {
+					_titleTagLastTextContent = document.querySelector("title").innerText; // stores the last text content just in case
+					document.querySelector("title").innerText = inputtext;
+				},
+				RenderDebugLines() {
+					if (document.querySelector("#render-debug-lines-css") == null) {
+						document.head.insertAdjacentHTML(
+							"beforeend",
+							`
+								<style id="render-debug-lines-css" type="text/css">
+									/* render "debug" lines - css */
+
+									:root {
+										--debug-line-color: green;
+									}
+
+									* {
+										border: 1px solid var(--debug-line-color);
+									}
+								</style>
+							`
+						);
+						return;
+					}
+					
+					if (document.querySelector("#render-debug-lines-css") != null) {
+						document.querySelector("#render-debug-lines-css").remove();
+						return;
+					}
 				}
 			},
 			// this is just a wrapper to display.theme object
-			display(themeIndex) {// clear out the theme setting
+			display_theme(themeIndex) {
+				// clear out the theme setting
 				if (themeIndex == 0) {
 					display.theme._clear();
+					localStorage.setItem("theme_type", "light");
+					return;
 				}
 				// darkmode theme
 				if (themeIndex == 1) {
 					display.theme.darkmode();
+					localStorage.setItem("theme_type", "darkmode");
+					return;
 				}
 				// darkmode theme
 				if (themeIndex == 2) {
 					display.theme.darkerdarkmode();
+					localStorage.setItem("theme_type", "darkerdarkmode");
+					return;
 				}
 				// black hole theme
 				if (themeIndex == 3) {
 					display.theme.blackhole();
+					localStorage.setItem("theme_type", "blackhole");
+					return;
 				}
 				// if for whatever reason, is called without a value
 				if (themeIndex == undefined || themeIndex == "" || themeIndex == null) {
 					console.log(`please provide an index number\n\n\t0 - clear out the theme\n\t1 - dark mode\n\t2 - darker dark mode\n\t3 - black hole`);
 				}
-			}
+			},
+			origAndroidClientVer: "17.03.38",
+			fakeAndroidClientVer: ""
 		}
 	}
 }
