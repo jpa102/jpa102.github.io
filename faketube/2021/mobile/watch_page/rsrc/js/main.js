afcc4f295516e0bf4c51521a0773b338565d50dc
John Patrick Adem <161832897+jpa102@users.noreply.github.com>
2026-04-28 05:21:03 +0800

    Refactor main.js to include video state management

--- faketube/2021/mobile/watch_page/rsrc/js/main.js
+++ faketube/2021/mobile/watch_page/rsrc/js/main.js
@@ -2,10 +2,62 @@
 	main.js
 */
 
+setTimeout(() => {
+	window.scrollTo({ top: 0 });
+}, 100);
+
+class _internal {
+	static _createVideoIdStateEntryArr() {
+		
+	}
+
+	static createVideoIdStateEntry(videoId) {
+		let m = new Map();
+		let n = new Map();
+
+		// video info container
+		m.set("", "");
+
+		// main video info
+		n.set("videoId", videoId);
+		n.set("videoState", "NEUTRAL");
+		n.set("isDownloaded", "NOT_DOWNLOADED");
+		n.set("isSaved", "NOT_SAVED");
+		n.set("videoTitle", global_data.yt.videoTitle);
+		n.set("channelName", global_data.yt.channelName);
+		n.set("channelLink", channelLink);
+		n.set("", );
+		n.set("", );
+	}
+}
+
+class buttonStates {
+	static likeButton(setState) {
+		
+	}
+
+	static dislikeButton(setState) {
+		
+	}
+
+	static downloadButton(setState) {
+		
+	}
+
+	static saveButton(setState) {
+		
+	}
+}
+
+// initialize the button states (like, dislike, download, save)
+if (localStorage.getItem("_video_state_by_videoids") == null) {
+	localStorage.setItem("_video_state_by_videoids", "");
+}
+
 // initialize the load related videos button setting
-// by default, it's set to "false"
+// by default, it's set to "none"
 if (localStorage.getItem("_settings_display_load_related_type") == null) {
-	localStorage.setItem("_settings_display_load_related_type", "false");
+	localStorage.setItem("_settings_display_load_related_type", "none");
 }
 
 // read the _settings_display_load_related_type value
@@ -14,13 +66,32 @@ if (localStorage.getItem("_settings_display_load_related_type") == null) {
 switch (localStorage.getItem("_settings_display_load_related_type")) {
 	case "button_type":
 		setTimeout(() => {
-			document.querySelector("#show-related-videos-button-container").removeAttribute("style");
+			document.querySelector("#show-related-videos-button").removeAttribute("style");
 		}, 1000);
 		break;
 	case "auto_type":
 		setTimeout(() => {
 			loadRelatedVideos(true, 4000);
 		}, 1000);
+		break;
+}
+
+// initialize the load upcoming video button setting
+// by default, it's set to "false"
+if (localStorage.getItem("_settings_display_load_upcoming_video") == null) {
+	localStorage.setItem("_settings_display_load_upcoming_video", "false");
+}
+
+// read the _settings_display_load_related_type value
+//		if set to true, display the button
+//		if set to false, don't display it
+switch (localStorage.getItem("_settings_display_load_upcoming_video")) {
+	case "true":
+		setTimeout(() => {
+			document.querySelector("#load-first-upcoming-video-button").removeAttribute("style");
+		}, 1000);
+		break;
+	case "false":
 		break;
 }
 
